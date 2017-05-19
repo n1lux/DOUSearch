de8fbaffdf37c4738c448a64659031509cfda52b
n1lux <n1lux.comp@gmail.com>
2017-05-19 09:56:09 -0300

    Change main function to class

--- dousearch/main.py
+++ dousearch/main.py
@@ -1,13 +1,24 @@
 import argparse, sys
 
 
-def main(argv):
-    parser = argparse.ArgumentParser(argv)
-    parser.add_argument('--foo', help='foo help')
-    args = parser.parse_args()
+class Main:
+    def __init__(self, argv):
+        parser = argparse.ArgumentParser(argv)
+        parser.add_argument('--foo', dest='foo', help='foo help')
+        parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+        args = parser.parse_args()
 
-    if args.foo:
-        print(args.foo)
+        if args.foo:
+            self.foo(args.foo)
+
+    @staticmethod
+    def foo(arg):
+        print(arg)
+
+
+def main(argv):
+    Main(argv)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
