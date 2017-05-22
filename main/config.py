bc2def348bec9310488c2c4244a57907c45b9755
n1lux <n1lux.comp@gmail.com>
2017-05-22 14:13:36 -0300

    Change temp files dir

--- main/config.py
+++ main/config.py
@@ -6,5 +6,6 @@ ROOT_DIR = Path(os.path.abspath(os.path.join(__file__, os.pardir))).parent
 VERSION = '1.0'
 SEARCH_ROOT = "http://portal.imprensanacional.gov.br/pesquisa"
 WEBDRIVER_PATH = os.path.join(ROOT_DIR, 'webdrivers/')
-TEMP_DIR = "/tmp/main/data/"
+TEMP_DIR = "/tmp/dousearch/data/"
 DEFAULT_WEBDRIVER = 1
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
